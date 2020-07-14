6a4f4ce354906aef97fa01922a420718f426a7f4
webdatarocksteam <info@webdatarocks.com>
2020-07-14 14:23:08 +0300

    ref example update

--- ES6/src/App.js
+++ ES6/src/App.js
@@ -1,15 +1,36 @@
 import React from 'react';
 
 import * as WebDataRocksReact from './webdatarocks.react';
+//import "webdatarocks/webdatarocks.highcharts";
 
-export class App extends React.PureComponent {
+export class App extends React.Component {
+
+  myRef = null;
+
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+
+  reportComplete = () => {
+    console.log(">>>>>", this.myRef.webdatarocks.getReport());
+  }
 
   render() {
 
     return (
 
     <div>
-      <WebDataRocksReact.Pivot toolbar={true} report="https://cdn.webdatarocks.com/reports/report.json"/>
+      <WebDataRocksReact.Pivot 
+        ref={(elem) => {
+          this.myRef = elem
+        }} 
+        toolbar={true} 
+        report="https://cdn.webdatarocks.com/reports/report.json" 
+        reportcomplete={() => {
+          this.reportComplete();
+        }}
+      />
     </div>
     );
   }
