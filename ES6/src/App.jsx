3728e457f11041f09b31a7253e6c71d59c09c9e9
avlare <ann.rechkalova@gmail.com>
2025-08-14 17:05:50 +0300

    small fixes
    fixes according to the comments (except readme)

--- ES6/src/App.jsx
+++ ES6/src/App.jsx
@@ -1,23 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
-import TopMenu from './components/TopMenu'
-import * as WebDataRocksReact from '@webdatarocks/react-webdatarocks';
+import "./App.css";
+import TopMenu from "./components/TopMenu";
+import * as WebDataRocksReact from "@webdatarocks/react-webdatarocks";
 
-class App extends Component {
-  render() {
-    return (
-      <div id="container">
-        <TopMenu />
-        <div id="pivot">
-          <WebDataRocksReact.Pivot
-            toolbar={true}
-            height="600"
-            report="https://cdn.webdatarocks.com/reports/report.json"
-          />
-        </div>
+function App() {
+  return (
+    <div id="container">
+      <TopMenu />
+      <div id="pivot">
+        <WebDataRocksReact.Pivot
+          toolbar={true}
+          height="600"
+          report="https://cdn.webdatarocks.com/reports/report.json"
+        />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,23 +1,20 @@
-import { Component } from 'react';
-import './App.css';
-import TopMenu from './components/TopMenu.tsx';
-import * as WebDataRocksReact from '@webdatarocks/react-webdatarocks';
+import "./App.css";
+import TopMenu from "./components/TopMenu.tsx";
+import * as WebDataRocksReact from "@webdatarocks/react-webdatarocks";
 
-class App extends Component {
-  render() {
-    return (
-      <div id="container">
-        <TopMenu />
-        <div id="pivot">
+function App() {
+  return (
+    <div id="container">
+      <TopMenu />
+      <div id="pivot">
         <WebDataRocksReact.Pivot
           toolbar={true}
           height="600"
           report="https://cdn.webdatarocks.com/reports/report.json"
         />
-        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
